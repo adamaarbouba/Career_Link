4642b01f2d35849fabbf6a55e7790dbd736de8db
adamaarbouba <adamaarbouba@gmail.com>
2026-01-21 14:32:04 +0100

    Router Fix

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,18 +1,23 @@
 <?php
+
 namespace App\Models;
 
-abstract class User {
-    protected $id;
-    protected $name;
-    protected $email;
-    protected $password;
-    protected $role;
+abstract class User
+{
+    protected ?int $id;
+    protected string $name;
+    protected string $email;
+    protected string $password;
+    protected string $role;
 
-    public function __construct($name, $email, $password) {
+    public function __construct($id = null, $name, $email, $password, $role)
+    {
+        $this->id = $id;
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
+        $this->role = $role;
     }
 
-    abstract public function setRole($role);
+    abstract public function setRole(Role $role);
 }
