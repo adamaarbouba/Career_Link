07335310daf656cf71eb848c98f29197bbd2a911
d-anasx <djamaouianas@gmail.com>
2026-01-27 09:05:40 +0100

    merged

--- src/Repositories/OfferRepository.php
+++ src/Repositories/OfferRepository.php
@@ -6,6 +6,12 @@ use App\Repositories\BaseRepository;
 class OfferRepository extends BaseRepository {
     protected $table = 'offers';
 
+    public function getAllWithRecruiter(){
+        $stmt = $this->conn->prepare("SELECT o.* ,r.company_name FROM offers o join recruteurs r on o.recruteur_id = r.id");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
     public function applicationsCountByOfferId(int $offerId)
     {
         $sql = "SELECT COUNT(*) FROM candidates_offers WHERE offer_id = :offer_id";
